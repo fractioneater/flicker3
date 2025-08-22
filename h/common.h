454f32c205381e39ba5afa34ea3b5786769c2828
fractioneater <98233032+fractioneater@users.noreply.github.com>
2025-08-21 21:08:25 -0400

    More nothing

---
+++ h/common.h
@@ -0,0 +1,25 @@
+#ifndef FLICKER_3_COMMON_H
+#define FLICKER_3_COMMON_H
+
+// DEBUG FLAGS
+
+// For the three following flags: 0 to disable, 1 to print user code, 2 to print everything (includes core module)
+// Don't compile; just print the tokens straight from the lexer.
+#define DEBUG_PRINT_TOKENS 0
+// Print bytecode instructions immediately after compiling.
+#define DEBUG_PRINT_CODE 0
+// Print bytecode instructions as they run.
+#define DEBUG_TRACE_EXECUTION 0
+
+// Run the GC whenever the VM or compiler messes with memory.
+#define DEBUG_STRESS_GC 0
+// Log memory management and garbage collector runs.
+#define DEBUG_LOG_GC 0
+
+// COMPILER AND VM VALUES
+
+#define MAX_PARAMETERS 16
+#define MAX_METHOD_NAME 64
+#define MAX_CONSTANTS 0x7fff
+
+#endif // FLICKER_3_COMMON_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,58 @@
+#include "vm.h"
+
 #include <iostream>
+#include <fstream>
 
 void repl() {
-  std::string line {"a"};
-  // Not the cleanest syntax, but this comma expression works to print the "~ >" prompt.
-  while (std::cout << "~ > ", std::getline(std::cin >> std::ws, line)) {
+  constexpr std::string_view prompt {"~ > "};
+  std::string line {};
+  // Not the cleanest syntax, but this comma expression works to print the "~ >" prompt and then get input.
+  while (std::cout << prompt, std::getline(std::cin >> std::ws, line)) {
+    // Normally this would interpret the code instead of spitting it back out.
     std::cout << "you typed " << line << '\n';
   }
 
-  // Clear the prompt characters from the last line.
-  std::cout << "\b\b\b\b";
+  // Clear the prompt characters from the last line by printing \b as many times as necessary.
+  std::cout << std::string(std::ssize(prompt), '\b');
 }
 
-int main(int argc, char* argv[]) {
+void run_file(const char* path) {
+  std::ifstream in {path};
+
+  if (!in) {
+    std::cerr << "Could not open file '" << path << "'\n";
+    throw std::system_error(74, std::iostream_category()); // Exit code 74: generic i/o failure.
+  }
+
+  // TODO: What about memory errors? How will they be reported?
+
+  // std::string str_input {};
+  // while (std::getline(in, str_input)) {
+  //   // Normally this would be added to some sort of longer source buffer to be run.
+  //   std::cout << str_input << '\n';
+  // }
+
+  interpret_result result = interpret(&in, "module");
+
+  // This will be done automatically when it goes out of scope, but it's good practice anyway.
+  in.close();
+
+  if (result == INTERPRET_COMPILE_ERROR) throw std::system_error(65, std::generic_category()); // Exit code 65: data format error (compile error).
+  if (result == INTERPRET_RUNTIME_ERROR) throw std::system_error(70, std::generic_category()); // Exit code 70: internal software error (runtime error).
+}
+
+int main(int argc, const char* argv[]) {
   if (argc == 1) {
     repl();
   } else if (argc == 2) {
-    // Hmmm
+    try {
+      run_file(argv[1]);
+    } catch (std::system_error& err) {
+      return err.code().value();
+    }
   } else {
     std::cout << "Usage: flicker [path]\n";
-    return 64;
+    return 64; // Exit code 64: command-line usage error.
   }
 
   return 0;
